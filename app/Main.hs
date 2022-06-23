8d92ae8b38d92cdf0d776aa49a6d4628e8c2662a
Winston Hartnett <winstonhartnett@gmail.com>
2022-06-23 04:20:06 -0500

    UnrollM

--- app/Main.hs
+++ app/Main.hs
@@ -7,6 +7,7 @@
 {-# LANGUAGE UndecidableInstances #-}
 {-# LANGUAGE AllowAmbiguousTypes #-}
 {-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
 
 module Main (main) where
 
@@ -32,10 +33,24 @@ instance Unroll 0 where
 instance {-# OVERLAPPABLE #-} Unroll (p - 1) => Unroll p where
   nTimes f x = f (nTimes @(p - 1) f x)
 
+-- New monadic definition
+{-# INLINE incAllM #-}
+incAllM :: Monad m => V.Vector Int -> m (V.Vector Int)
+incAllM = pure . V.map (+ 1)
+
+class UnrollM (n :: Nat) where
+  nTimesM :: Monad m => (a -> m a) -> a -> m a
+
+instance UnrollM 0 where
+  nTimesM f x = pure x
+
+instance {-# OVERLAPPABLE #-} UnrollM (p - 1) => UnrollM p where
+  nTimesM f x = f =<< nTimesM @(p - 1) f x
+
 main :: IO ()
 main = do
   let size = 100000000 :: Int
   let array = V.replicate size 0 :: V.Vector Int
-  print $ V.sum (nTimes @64 incAll array)
   -- print $ V.sum (oldNTimes 64 incAll array)
-
+  -- print $ V.sum (nTimes @64 incAll array)
+  print . V.sum =<< nTimesM @64 incAllM array
